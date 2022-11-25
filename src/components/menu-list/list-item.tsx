563804435021ecb6da5967da4d18cda47cc031eb
Geekles <tondjilee@gmail.com>
2022-11-26 00:20:31 +0100

    responsive

--- src/components/menu-list/list-item.tsx
+++ src/components/menu-list/list-item.tsx
@@ -5,9 +5,9 @@ type ListItemProps = {}
 const ListItem = ({}: ListItemProps) => {
     const money = 4000;
 
-    return <div className={"grid grid-cols-12 font-heading"}>
-        <div className="col-span-2 flex items-center justify-center border">{money.toLocaleString('en-US', {maximumFractionDigits:2})} XAF</div>
-        <div className="col-span-10 p-8 border">Beef∙Chipotle-Beer-Beef + Smokey Peppers</div>
+    return <div className={"flex font-heading"}>
+        <div className="w-24 flex items-center justify-center border">{money.toLocaleString('en-US', {maximumFractionDigits:2})} XAF</div>
+        <div className="flex-1 p-8 border">Beef∙Chipotle-Beer-Beef + Smokey Peppers</div>
     </div>
 
 }
